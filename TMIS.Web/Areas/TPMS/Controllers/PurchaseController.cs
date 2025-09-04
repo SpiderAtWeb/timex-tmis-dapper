8cacf59978069fb6f272c9d935455a938f24dac9
CosthaAtWeb <sujithacostha@outlook.com>
2025-09-04 17:26:37 +0530

    changes done

---
+++ TMIS.Web/Areas/TPMS/Controllers/PurchaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TMIS.Areas.TPMS.Controllers
+{
+  public class PurchaseController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
